a70018764190ff732199b69e116eb1b8a13ead49
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2012-03-19 21:27:44 -0400

    first spec, accepting a yaml file and setting title

--- lib/work.rb
+++ lib/work.rb
@@ -1,2 +1,9 @@
 class Work
+  attr_accessor :title
+
+  def initialize yaml_data
+    yaml_data.each do |key, value|
+      self.send "#{key}=".intern, value
+    end
+  end
 end
--- spec/work_spec.rb
+++ spec/work_spec.rb
@@ -1,5 +1,11 @@
 require 'work'
 
 describe Work do
-  it "accepts a YAML file"
+  before do
+    @data = YAML.load_file "./spec/fixtures/work_sample.yml"
+    @sample = Work.new @data
+  end
+  it "fetches its title" do
+    @sample.title.should_not be_nil
+  end
 end
