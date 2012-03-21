f2206b9fcc51c80fafbe4a30d9393c2dd5217fc1
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2012-03-20 20:53:50 -0400

    fetching all from data folder

--- lib/work.rb
+++ lib/work.rb
@@ -1,4 +1,12 @@
 require 'yaml_reader'
 
+WORK_DIRECTORY = "./data/work"
+
 class Work < YAMLReader
+
+  def self.all
+    all_files = Dir.glob "#{ WORK_DIRECTORY }/*.yml"
+    all_files.map {|path| Work.new path }
+  end
+
 end
--- spec/work_spec.rb
+++ spec/work_spec.rb
@@ -6,7 +6,7 @@ describe Work do
     @sample = Work.new "./spec/fixtures/work_sample.yml"
   end
 
-  context "from its configuration" do
+  context "from its yaml file" do
     it "fetches its title" do
       @sample.title.should_not be_nil
     end
@@ -15,6 +15,25 @@ describe Work do
     end
     it "fetches an array of roles" do
       @sample.roles.class.should be Array
+    end
+  end
+
+  context "self.all" do
+
+    before do
+      @works = Work.all
+    end
+
+    it "fetches all work samples" do
+      @works.should_not be_nil
+    end
+
+    it "returns an array" do
+      @works.class.should be Array
+    end
+
+    it "returns an array containing instantited Work objects" do
+      @works.first.class.should be Work
     end
   end
 
