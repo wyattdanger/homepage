f45ce9e974e99898dd656c1efa7f59c04fe441b8
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2012-04-04 22:53:37 -0400

    refactoring work.rb

--- lib/work.rb
+++ lib/work.rb
@@ -6,17 +6,11 @@ class Work < YAMLReader
 
   class WorkNotFoundException < Exception; end
 
-  def self._all_files
-    Dir.glob "#{ WORK_DIRECTORY }/*.yml"
-  end
-
   def self.all
-    all_files = Work._all_files
     all_files.map {|path| Work.new path }
   end
 
   def self.find slug
-    all_files = Work._all_files
     desired_file = all_files.select {|x| Work.new(x).slug == slug }.first
     unless desired_file.nil?
       Work.new desired_file
@@ -27,6 +21,12 @@ class Work < YAMLReader
 
   def slug
     @slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+  end
+
+  private
+
+  def self.all_files
+    @all_files ||= Dir.glob "#{ WORK_DIRECTORY }/*.yml"
   end
 
 end
